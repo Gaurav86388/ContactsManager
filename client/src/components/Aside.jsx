1d467191588f5dfd4ab942db93f755fbf1284652
Gaurav Gogoi <gauravgogoigp@gmail.com>
2024-03-19 01:39:53 +0530

    designing phase over

--- client/src/components/Aside.jsx
+++ client/src/components/Aside.jsx
@@ -2,7 +2,17 @@ import React,{memo} from 'react'
 import "./Aside.css"
 import dashboardicon from '/dashboardicon.svg'
 import contactsicon from '/contactsicon.svg'
+import logout from "/logout.svg"
+import {useNavigate} from 'react-router-dom'
+
 const Aside = memo(function Aside (){
+const navigate = useNavigate()
+
+
+  function handleLogout(){
+    localStorage.removeItem("jwt")
+    navigate("/")
+  }
   return (
     <div className='aside'>
           <h3>Logo</h3>
@@ -16,6 +26,14 @@ const Aside = memo(function Aside (){
             <img src={contactsicon} alt="contactsicon" />
             <input type="button" value="Total contacts" id="total-contacts-toggle"/>
             </div>
+            
+          </div>
+
+          <div className="logout-section">
+            <button className='logout-button' onClick={handleLogout}>
+            <img src={logout} alt="logout image" />
+              Logout
+            </button>
             
           </div>
     </div>
---
+++ client/src/components/TableData.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { dummyData } from "../demodata.js";
+import "./TableData.css";
+
+import bin from "/bin.svg";
+import editPencil from "/editpencil.svg";
+import upboldarrow from "/upboldarrow.svg";
+import downboldarrow from "/downboldarrow.svg";
+
+const tableHeadings = [
+  "Name",
+  "Designation",
+  "Company",
+  "Industry",
+  "Email",
+  "Phone number",
+  "Country",
+  "Action",
+];
+
+const TableData = () => {
+  return (
+    <>
+      <table className="records-data">
+        <thead>
+          <tr>
+            {tableHeadings.map((item, index) => {
+              return (
+                <th className={item} key={item}>
+                  <div className="table-heading-names">
+                    {index === 0 && (
+                      <input
+                        type="checkbox"
+                        name="colCheckbox"
+                        id="first-col-checkbox"
+                      />
+                    )}
+                    <span>{item}</span>
+
+                    {index === 1 || index === 2 || index === 3 ? (
+                      <div className="arrows-images">
+                        <img src={upboldarrow} alt="up arrow" />
+                        <img src={downboldarrow} alt="down arrow" />
+                      </div>
+                    ) : null}
+                  </div>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {dummyData.map((item, index) => {
+            return (
+              <tr className="data-row" key={item.Name}>
+                <td>
+                  <div className="first-col">
+                    <input
+                      type="checkbox"
+                      name="colCheckbox"
+                      id="first-col-checkbox"
+                    />
+                    <span>{item.Name}</span>
+                  </div>
+                </td>
+                <td>{item.Designation}</td>
+                <td>{item.Company}</td>
+                <td>{item.Industry}</td>
+                <td>{item.Email}</td>
+                <td>{item.PhoneNumber}</td>
+                <td>{item.Country}</td>
+                <td>
+                  <div className="action-icons">
+                    <button className="edit-row">
+                      <img src={editPencil} alt="pencil edit" />
+                    </button>
+
+                    <button className="delete-row">
+                      <img src={bin} alt="bin" />
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+export default TableData;
